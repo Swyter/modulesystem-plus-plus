9f2427fbfd696d7210698a5085312fc63366cbd0
Alessio Sgarro <cmp-@users.noreply.github.com>
2011-05-13 20:42:15 +0100

    Use operator overloading instead of CPyObject::GetItem, several structure changes.

---
+++ MS++/StringUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+
+std::string &ltrim(std::string &str, const std::string &chars = " \t\n\v\f\r");
+std::string &rtrim(std::string &str, const std::string &chars = " \t\n\v\f\r");
+std::string &trim(std::string &str, const std::string &chars = " \t\n\v\f\r");
+std::string &lower(std::string &str);
+std::string &replace(std::string &str, char character, char replacement);
+std::string &remove(std::string &str, char character);
--- MS++/cMS.cpp
+++ MS++/cMS.cpp
@@ -5,25 +5,9 @@
 
 int main(int argc, char **argv)
 {
-	Py_Initialize();
-	
-	ModuleSystem ms;
-	LARGE_INTEGER frequency, t1, t2;
-	
-	QueryPerformanceFrequency(&frequency);
-	QueryPerformanceCounter(&t1);
+	ModuleSystem ms("D:\\Dev\\test");
 
-	try
-	{
-		ms.Compile();
-	}
-	catch (CompileException e)
-	{
-		std::cout << "Exception: " << e.GetText() << std::endl;
-	}
-
-	QueryPerformanceCounter(&t2);
-	std::cout << "Compile time: " << ((t2.QuadPart - t1.QuadPart) * 1000.0 / frequency.QuadPart) << "ms" << std::endl;
+	ms.Compile();
 	
 	system("pause");
 }
